182160186f041968791708cfcc5921a72f0b1a03
hdz-kevin <kevinhdez592@gmail.com>
2025-12-14 18:21:22 -0600

    first route using a controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BackendController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
     return 'GET /test OK';
 });
+
+Route::get('/backend', [BackendController::class, 'get']);
